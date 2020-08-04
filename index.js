b10b0a398b57bb2128f6e4d89797418b592e7c28
mail@nixiaobai.com <mail@nixiaobai.com>
2020-08-04 09:42:13 +0800

    项目初始化

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+exports.main_handler = async (event, context, callback) => {
+    console.log("Hello World")
+    console.log(event)
+    console.log(event["non-exist"])
+    console.log(context)
+    return event
+};
+
